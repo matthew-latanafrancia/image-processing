2ab65fcef0f24d652c38d435139db1b8fa70b6e1
dettwij <dettwij@wwu.edu>
2022-03-01 14:56:27 -0800

    Implemented multithreading and laplacian outputs properly

--- imath.c
+++ imath.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-#define THREADS 4
+#define THREADS 1
 
 #define FILTER_WIDTH 3
 #define FILTER_HEIGHT 3
@@ -25,6 +25,7 @@ struct parameter {
 	unsigned long int size;  //equal share of work (almost equal if odd)
 };
 
+pthread_barrier_t our_barrier;
 
 /*This is the thread function. It will compute the new values for the region of image specified in params (start to start+size) using convolution.
     (1) For each pixel in the input image, the filter is conceptually placed on top ofthe image with its origin lying on that pixel.
@@ -41,8 +42,7 @@ void *threadfn(void *params)
 	  -1,  8, -1,
 	  -1, -1, -1,
 	};
-  
-  printf("You are here\n");
+  int counter = 0;
   int red, green, blue;
   int x_coordinate = 0;
   int y_coordinate = 0;
@@ -50,15 +50,8 @@ void *threadfn(void *params)
   int iteratorImageHeight = 0;
   int iteratorFilterWidth = 0;
   int iteratorFilterHeight = 0;
-
-  //printf("Calculation: %ld\n", (iteratorImageWidth - (FILTER_WIDTH / 2) + iteratorFilterWidth + p->w) % p->w);
-  /*x_coordinate = (iteratorImageWidth - (FILTER_WIDTH / 2) + iteratorFilterWidth + p->w) % p->w;
-  y_coordinate = (iteratorImageHeight - (FILTER_HEIGHT / 2) + iteratorFilterHeight + p->h) % p->h;
-  red += p->image[y_coordinate * p->w + x_coordinate].r * laplacian[iteratorFilterHeight][iteratorFilterWidth];
-  green += p->image[y_coordinate * p->w + x_coordinate].g * laplacian[iteratorFilterHeight][iteratorFilterWidth];
-  blue += p->image[y_coordinate * p->w + x_coordinate].b * laplacian[iteratorFilterHeight][iteratorFilterWidth];*/
   
-  for(iteratorImageHeight = 0; iteratorImageHeight < p->h; iteratorImageHeight++)
+  for(iteratorImageHeight = p->start; iteratorImageHeight < p->size; iteratorImageHeight++)
   {
     for(iteratorImageWidth = 0; iteratorImageWidth < p->w; iteratorImageWidth++)
     {
@@ -114,14 +107,12 @@ void *threadfn(void *params)
         {
           p->result[iteratorImageHeight * p->w + iteratorImageWidth].b = blue;
         }
+        // printf("%d\n", counter);
+        // counter++;
       }
     }
-
+    //printf("%d\n", iteratorImageHeight);
   }
-  //We need to cast the PPMPixel chars to ints
-  //red+= image[y_coordinate * imageWidth + x_coordinate].r * laplacian[iteratorFilterHeight][iteratorFilterWidth]
-  //green+= image[y_coordinate * imageWidth + x_coordinate].g * laplacian[iteratorFilterHeight][iteratorFilterWidth]
-  //blue+= image[y_coordinate * imageWidth + x_coordinate].b * laplacian[iteratorFilterHeight][iteratorFilterWidth]
 
   /*For all pixels in the work region of image (from start to start+size)
     Multiply every value of the filter with corresponding image pixel. Note: this is NOT matrix multiplication.
@@ -129,7 +120,7 @@ void *threadfn(void *params)
    //truncate values smaller than zero and larger than 255
     Store the new values of r,g,b in p->result.
    */
-		
+	pthread_barrier_wait(&our_barrier);	
 	return NULL;
 }
 
@@ -182,45 +173,7 @@ void writeImage(PPMPixel *image, char *name, unsigned long int width, unsigned l
       fclose(fp);
       exit(1);
     }
-    /*
-    widthCounter++;
-    if(widthCounter == width){
-      widthCounter = 0;
-      fprintf(fp, "\n");
-    }*/
   }
-  /*
- printf("%ld\n", sizeof(PPMPixel));
-  unsigned char **temp = (unsigned char**)malloc(sizeof(char*) * height);
-  int indexCounter = 0;
-  int tempCounter = 0;
-  temp[tempCounter] = (unsigned char *)malloc(sizeof(char) * width * 3);
-  for(int i = 0; i < (height*width); i++){
-    counter++;
-    printf("Putting in pixel: %d\nr: %d\ng: %d\nb: %d\n", counter, image[i].r, image[i].g, image[i].b);
-    temp[tempCounter][indexCounter] = image[i].r;
-    indexCounter++;
-    temp[tempCounter][indexCounter] = image[i].g;
-    indexCounter++;
-    temp[tempCounter][indexCounter] = image[i].b;
-    indexCounter++;
-
-    if(indexCounter == (width * 3)){
-      tempCounter++;
-      temp[tempCounter] = (char *)malloc(sizeof(char) * width * 3);
-      indexCounter = 0;
-    }
-  }
-
-  check = fwrite(temp, sizeof(temp), 1, fp);
-    if(check == 0){
-      //error
-      printf("Error in writeImage\n");
-      fclose(fp);
-      exit(1);
-    }
-
-  */
   fclose(fp);
 }
 /* Open the filename image for reading, and parse it.
@@ -239,7 +192,7 @@ void writeImage(PPMPixel *image, char *name, unsigned long int width, unsigned l
 PPMPixel *readImage(const char *filename, unsigned long int *width, unsigned long int *height)
 {
 
-  PPMPixel *img;
+  //PPMPixel *img;
   int check;
   int fileMaxRGB;
   char input;
@@ -297,7 +250,7 @@ PPMPixel *readImage(const char *filename, unsigned long int *width, unsigned lon
   ungetc((int)input, fp);
   fscanf(fp, "%ld", width);
   fscanf(fp, "%ld", height);
-  printf("%ld   %ld\n", *width, *height);
+  //printf("%ld   %ld\n", *width, *height);
 
   //check for max byte
   //Read rgb component.  Check if it is equal toman pthread RGB_MAX. If not, display error message.
@@ -306,7 +259,7 @@ PPMPixel *readImage(const char *filename, unsigned long int *width, unsigned lon
   if(check == 0)
     printf("error first\n");
 
-  printf("%d\n", fileMaxRGB);
+  //printf("%d\n", fileMaxRGB);
     
   //allocate memory for img. NOTE: A ppm image of w=200 and h=300 will contain 60000 triplets (i.e. for r,g,b), ---> 18000 bytes.
 
@@ -352,18 +305,38 @@ PPMPixel *apply_filters(PPMPixel *image, unsigned long w, unsigned long h, doubl
   PPMPixel *result;
   pthread_t aThread;
   void *tretA = 0;
-
+  clock_t begin = clock();
   //allocate memory for result
   result = (PPMPixel*)malloc(sizeof(PPMPixel) * h * w);
-  struct parameter a;
-  a.image = image;
+  struct parameter params[THREADS];
+  /*a.image = image;
   a.result = result;
   a.w = w;
   a.h = h;
   a.start = 0;
-  a.size = w * h;
-
-  if(pthread_create(&aThread, NULL, threadfn, (void*)&a) != 0)
+  a.size = w * h;*/
+  pthread_t threadArray[THREADS];
+  pthread_barrier_init(&our_barrier, NULL, THREADS);
+  for(int i = 0; i < THREADS; i++)
+  {
+    params[i].image = image;
+    params[i].result = result;
+    params[i].w = w;
+    params[i].h = h;
+    params[i].start = (h/THREADS) * i;
+    params[i].size = h/THREADS * (i + 1);
+    //(h * w)/THREADS * (i + 1);
+    if(pthread_create(&threadArray[i], NULL, threadfn, (void*)&params[i]) != 0)
+    {
+      printf("ERROR CREATING THREAD\n");
+      exit(1);
+    }
+  }
+  for(int i = 0; i < THREADS; i++)
+  {
+    pthread_join(threadArray[i], NULL);
+  }
+  /*if(pthread_create(&aThread, NULL, threadfn, (void*)&a) != 0)
   {
     printf("ERROR CREATING THREAD\n");
     exit(1);
@@ -372,29 +345,10 @@ PPMPixel *apply_filters(PPMPixel *image, unsigned long w, unsigned long h, doubl
   {
     pthread_join(aThread, &tretA);
     printf("Thread created\n");
-  }
-  
-  printf("Width: %ld\n", a.w);
-  printf("Height: %ld\n", a.h);
-  printf("Start: %ld\n", a.start);
-  printf("Size: %ld\n", a.size);
-
-  //allocate memory for parameters (one for each thread)
-  //How many threads do we use?
-
-  /*create threads and apply filter.
-   For each thread, compute where to start its work.  Determine the size of the work. If the size is not even, the last thread shall take the rest of the work.
-   */
-  
-  int size = (w * h)/4;
-
-   //Let threads wait till they all finish their work.
-   //We would probably use the barrier here
-
-  /*Start idea is that we have each thread take a quarter of the image
-   *and use that as its size parameter. The formula is (w * h)/# of threads 
-   */
-
+  }*/
+  pthread_barrier_destroy(&our_barrier);
+  clock_t end = clock();
+  *elapsedTime = (double)(end - begin)/CLOCKS_PER_SEC;
 	return result;
 }
 
@@ -425,6 +379,6 @@ int main(int argc, char *argv[])
     //test image
     char* name = "laplacian.ppm";
     writeImage(finalImg, name, w, h);
-
+    printf("%f\n", elapsedTime);
 	return 0;
 }
