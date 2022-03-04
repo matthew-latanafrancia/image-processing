9c5d25ef15a5aa76a26645dfb8a8c261076af4b8
Matthew Latanafrancia <latanam@CF165-15.cs.wwu.edu>
2022-03-04 09:29:14 -0800

    Set up testing for our experiment and fixed the timer

--- imath.c
+++ imath.c
@@ -5,7 +5,7 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-#define THREADS 40
+#define THREADS 4
 
 #define FILTER_WIDTH 3
 #define FILTER_HEIGHT 3
@@ -228,7 +228,6 @@ PPMPixel *readImage(const char *filename, unsigned long int *width, unsigned lon
   ungetc((int)input, fp);
   fscanf(fp, "%ld", width);
   fscanf(fp, "%ld", height);
-  //printf("%ld   %ld\n", *width, *height);
 
   //check for max byte
   //Read rgb component.  Check if it is equal toman pthread RGB_MAX. If not, display error message.
@@ -237,13 +236,9 @@ PPMPixel *readImage(const char *filename, unsigned long int *width, unsigned lon
   if(check == 0)
     printf("error first\n");
 
-  //printf("%d\n", fileMaxRGB);
-    
-  //allocate memory for img. NOTE: A ppm image of w=200 and h=300 will contain 60000 triplets (i.e. for r,g,b), ---> 18000 bytes.
-
   //read pixel data from filename into img. The pixel data is stored in scanline order from left to right (up to bottom) in 3-byte chunks (r g b values for each pixel) encoded as binary numbers.
-
   PPMPixel* pix;
+  //allocate memory for img. NOTE: A ppm image of w=200 and h=300 will contain 60000 triplets (i.e. for r,g,b), ---> 18000 bytes.
   pix = (PPMPixel*)malloc(sizeof(PPMPixel) * (*height) * (*width));
 
   //Takes the rgb values of each pixel and stores them into the array pix
@@ -253,14 +248,6 @@ PPMPixel *readImage(const char *filename, unsigned long int *width, unsigned lon
     pix[i].g = fgetc(fp);
     pix[i].b = fgetc(fp);
   }
-
-  //Print statement to check if values are printed
-  /*for(int i = 0; i < ((*width) * (*height)); i++)
-  {
-    counter++;
-    printf("%d %d %d %d\n", pix[i].r, pix[i].g, pix[i].b, counter);
-  }*/
-  
   fclose(fp);
   return pix;
 }
@@ -273,14 +260,11 @@ PPMPixel *readImage(const char *filename, unsigned long int *width, unsigned lon
 PPMPixel *apply_filters(PPMPixel *image, unsigned long w, unsigned long h, double *elapsedTime) {
 
   PPMPixel *result;
-  //clock_t begin = clock(); //begin timer
   struct timeval t1, t2;
   gettimeofday(&t1, NULL);
-  //allocate memory for result
   result = (PPMPixel*)malloc(sizeof(PPMPixel) * h * w);
   struct parameter params[THREADS];
   pthread_t threadArray[THREADS];
-  //pthread_barrier_init(&our_barrier, NULL, THREADS);
   for(int i = 0; i < THREADS; i++)
   {
     //initialize individual values for the threads
@@ -297,8 +281,6 @@ PPMPixel *apply_filters(PPMPixel *image, unsigned long w, unsigned long h, doubl
     {
       params[i].size = h/THREADS * (i + 1);
     }
-    //params[i].size = h/THREADS * (i + 1);
-    //(h * w)/THREADS * (i + 1);
     if(pthread_create(&threadArray[i], NULL, threadfn, (void*)&params[i]) != 0)
     {
       printf("ERROR CREATING THREAD\n");
@@ -311,15 +293,8 @@ PPMPixel *apply_filters(PPMPixel *image, unsigned long w, unsigned long h, doubl
     //join the threads
     pthread_join(threadArray[i], NULL);
   }
-
-  // //destroy the barrier
-  // pthread_barrier_destroy(&our_barrier);
-  //clock_t end = clock(); //end clock
-  //*elapsedTime = (double)(end - begin)/CLOCKS_PER_SEC; //calculate time
   gettimeofday(&t2, NULL);
-
-  *elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
-  *elapsedTime = (t2.tv_usec - t1.tv_usec) / 100000.0;   // us to ms
+  *elapsedTime = (t2.tv_sec - t1.tv_sec) + ((t2.tv_usec - t1.tv_usec)/1000000.0);
 	return result;
 }
 
@@ -337,15 +312,30 @@ int main(int argc, char *argv[])
     {
       printf("Usage ./imath filename\n");
     }
-    PPMPixel* img = readImage(argv[1], &w, &h);
-    PPMPixel* finalImg = apply_filters(img, w, h, &elapsedTime);
 
-    //test image
-    char* name = "laplacian.ppm";
-    writeImage(finalImg, name, w, h);
-    printf("%.3f\n", elapsedTime);
+    int i = 0;
+    double totalTime;
+    double averageTime;
 
-    free(img);
-    free(finalImg);
+    while(i < 50){
+      PPMPixel* img = readImage(argv[1], &w, &h);
+      PPMPixel* finalImg = apply_filters(img, w, h, &elapsedTime);
+      if(elapsedTime > 0){
+        totalTime += elapsedTime;
+        printf("elapsedTime: %.3f\n", elapsedTime);
+        i++;
+      }
+
+      elapsedTime = 0;
+
+      char* name = "laplacian.ppm";
+      writeImage(finalImg, name, w, h);
+
+      free(img);
+      free(finalImg);
+    }
+    averageTime = totalTime/50;
+    printf("Average time over 50 runs: %.3f\n", averageTime);
+
 	return 0;
 }
